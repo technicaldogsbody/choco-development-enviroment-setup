8cd8aba74b8051c00afb9618c2241e606a151d21
Andy Blyth <andy@technicaldogsbody.com>
2024-04-17 12:17:37 +0100

    Create recipe_prep.ps1

---
+++ recipe_prep.ps1
@@ -0,0 +1,16 @@
+# choco installation
+# https://chocolatey.org/install
+
+Write-Host "=== Creating your development environment! ==="
+
+Write-Host "====> Installing Choco packages..."
+choco --version
+choco feature enable -name=exitOnRebootDetected
+choco feature enable -name=allowGlobalConfirmation;
+choco install ChocolateyGUI -y
+
+# windows subsystem for linux
+Write-Host "====> Installing windows subsystem for linux..."
+#choco list --source windowsfeatures
+choco install VirtualMachinePlatform -y -source windowsfeatures
+choco install Microsoft-Windows-Subsystem-Linux -y -source windowsfeatures
